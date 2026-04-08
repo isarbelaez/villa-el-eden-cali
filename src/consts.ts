479b80464be0980b31d1ffb2b6ccaa6e38458db9
Isabella Arbelaez <bellaarbelaez01@gmail.com>
2026-04-08 10:32:14 -0500

    corregir problema de imagenes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Villa Eucalipto Y Romero | Glamping & Cabañas';
+export const SITE_TITLE = 'Villa del Eden | Glamping & Cabañas';
 export const SITE_DESCRIPTION = 'Glamping y cabañas en la naturaleza. Suites con jacuzzi, vistas panorámicas y experiencias de descanso inolvidables. Reserva tu escapada.';
