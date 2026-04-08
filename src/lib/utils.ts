c08bd8df92d9e642fd235c025590d5eaa6b76bca
Isabella Arbelaez <bellaarbelaez01@gmail.com>
2026-04-08 13:24:12 -0500

    arreglar imagenes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getImageUrl = (img: string) => {
+  if (!img) return "";
+
+  // Si ya es una URL externa → NO tocar
+  if (img.startsWith("http")) {
+    return img;
+  }
+
+  // Si es local → agregar base
+  return `${import.meta.env.BASE_URL}${img.replace(/^\/+/, "")}`;
+};
